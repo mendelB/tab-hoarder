536a17cc73a6dc98f51b928aea1fde5f6f96d989
mendelB <mendelblesofsky@gmail.com>
2020-03-01 16:46:25 -0500

    Display tab favicon

--- resources/views/tabs/index.blade.php
+++ resources/views/tabs/index.blade.php
@@ -3,6 +3,7 @@
 <ol>
 	@foreach ($tabs as $tab)
 		<li>
+			<img src="https://www.google.com/s2/favicons?domain={{optional(parse_url($tab->url))['host']}}"/>
 			<a href="{{$tab->url}}" target="_blank" rel="noopener noreferrer">
 				{{ $tab->title ?: $tab->url }}
 			</a>
